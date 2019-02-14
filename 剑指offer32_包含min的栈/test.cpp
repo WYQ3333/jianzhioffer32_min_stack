5e889b64fedd1069985d9f14c7965004c4f14354
浅忆梦微凉 <3538922590@qq.com>
2019-02-14 21:04:26 +0800

    还是同种方法

--- 剑指offer32_包含min的栈/test.cpp
+++ 剑指offer32_包含min的栈/test.cpp
@@ -44,8 +44,57 @@ void TestFunc(){
 	cout << s.min() << endl;
 }
 
+//思路：利用一个辅助栈来存放最小值
+//
+//栈  3，4，2，5，1
+//辅助栈 3，3，2，2，1
+//每入栈一次，就与辅助栈顶比较大小，如果小就入栈，如果大就入栈当前的辅助栈顶
+//当出栈时，辅助栈也要出栈
+//这种做法可以保证辅助栈顶一定都当前栈的最小值
+
+class Solution2 {
+public:
+
+	stack<int> stack1, stack2;
+
+	void push(int value) {
+		stack1.push(value);
+		if (stack2.empty())
+			stack2.push(value);
+		else if (value <= stack2.top())
+		{
+			stack2.push(value);
+		}
+	}
+
+	void pop() {
+		if (stack1.top() == stack2.top())
+			stack2.pop();
+		stack1.pop();
+
+	}
+
+	int top() {
+		return stack1.top();
+	}
+
+	int min() {
+		return stack2.top();
+	}
+
+};
+
+void TestFunc2(){
+	Solution s;
+	s.push(2);
+	s.push(1);
+	s.push(4);
+	s.push(3);
+	s.push(5);
+	cout << s.min() << endl;
+}
 int main(){
-	TestFunc();
+	TestFunc2();
 	system("pause");
 	return 0;
 }
